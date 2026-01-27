4b168a04a4b0ab89732c3b2ff814720c60d7bf50
jobin2005 <ajjobin2005@gmail.com>
2026-01-28 00:01:26 +0530

    jobs frst

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.get("/health", (req, res) => {
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`🚀 Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
